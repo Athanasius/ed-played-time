e7f7c6636b2ec614430de7063c36d8358641ee78
Athanasius <github@miggy.org>
2020-05-03 11:38:21 +0100

    Imports initial script
     Implements very simple "total up the time difference between first
    and last lines of each Journal file found in the standard location".

---
+++ ed-played-time.ps1
@@ -0,0 +1,96 @@
+###########################################################################
+# PowerShell script to look through current Elite Dangerous 'Journal' files
+# and account apparent playtime.
+#
+# TODO:
+#   Current aim is to just look at timestamps of first and last line in
+#   each Journal file and total up the time deltas.  What we actually need
+#   to do is look at Location (written on login) events and either Shutdown
+#   events or the last line of the file.
+###########################################################################
+
+###########################################################################
+# C# code to interpret 'shell:' style paths, pared down to only SavedGames
+# <https://stackoverflow.com/a/25094236>
+###########################################################################
+Add-Type @"
+	using System;
+	using System.Runtime.InteropServices;
+
+	public static class KnownFolder {
+		public static readonly Guid SavedGames = new Guid( "4C5C32FF-BB9D-43b0-B5B4-2D72E54EAAA4" );
+	}
+
+	public class shell32 {
+		[DllImport("shell32.dll")]
+        	private static extern int SHGetKnownFolderPath(
+             	[MarshalAs(UnmanagedType.LPStruct)] 
+             	Guid rfid,
+             	uint dwFlags,
+             	IntPtr hToken,
+             	out IntPtr pszPath
+         	);
+
+         	public static string GetKnownFolderPath(Guid rfid)
+         	{
+            	IntPtr pszPath;
+            	if (SHGetKnownFolderPath(rfid, 0, IntPtr.Zero, out pszPath) != 0)
+                	return ""; // add whatever error handling you fancy
+            	string path = Marshal.PtrToStringUni(pszPath);
+            	Marshal.FreeCoTaskMem(pszPath);
+            	return path;
+         	}
+	}
+"@
+###########################################################################
+
+###########################################################################
+# Parse Journal timestamp
+###########################################################################
+$parse_timestamp = {
+	[cmdletbinding()]
+	param(
+		[parameter(
+			Mandatory = $true,
+			ValueFromPipeline = $true)
+		]
+		$ts
+	)
+	[datetime]::Parse($ts.timestamp.ToString())
+}
+###########################################################################
+
+###########################################################################
+# Parse a Journal file for the relevant events
+###########################################################################
+$parse_journal = {
+	[cmdletbinding()]
+	param(
+		[parameter(
+			Mandatory = $true,
+			ValueFromPipeline = $true)
+		]
+		$infile
+	)
+	Process {
+		Write-Host "parse_journal $infile"
+		$starttime = Get-Content -Path "$JournalFolder\$infile" -TotalCount 1 | ConvertFrom-Json | Select-Object -Property timestamp | &$parse_timestamp
+		$endtime = Get-Content -Path "$JournalFolder\$infile" -Tail 1 | ConvertFrom-Json | Select-Object -Property timestamp | &$parse_timestamp
+		Write-Host "start: $starttime"
+		Write-Host "end: $endtime"
+		$diff = $endtime - $starttime
+		Write-Host "diff: $diff"
+		$total_playedtime += $diff
+		Write-Host "total played now: $total_playedtime"
+	}
+}
+###########################################################################
+
+###########################################################################
+# MAIN code
+###########################################################################
+$total_playedtime = 0
+$SavedGames = [shell32]::GetKnownFolderPath([KnownFolder]::SavedGames)
+$JournalFolder = "$SavedGames\Frontier Developments\Elite Dangerous"
+Get-ChildItem "$JournalFolder" -Filter "Journal.*.*.log" | &$parse_journal
+###########################################################################
